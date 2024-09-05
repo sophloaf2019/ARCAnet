e2f15df922a13abe04a7c82ac53e30c4b8bdbc23
sophloaf2019 <sophloaf2019@proton.me>
2024-09-05 00:01:26 -0700

    test

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     RECENT_LOGS_COUNT = int(os.environ.get('RECENT_LOGS_COUNT', 5))
+    SERVER_NAME = "159.223.202.173:5000"
